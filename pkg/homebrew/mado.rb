e13cb6785caf82833bc2a15fd81456f4bef6bd8d
Akiomi KAMAKURA <akiomik@gmail.com>
2026-07-22 14:45:04 +0900

    chore: bump packages to v0.3.1 (#352)

--- pkg/homebrew/mado.rb
+++ pkg/homebrew/mado.rb
@@ -3,30 +3,30 @@
 class Mado < Formula
   desc "Fast Markdown linter written in Rust"
   homepage "https://github.com/akiomik/mado"
-  version "0.3.0"
+  version "0.3.1"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
       url "https://github.com/akiomik/mado/releases/download/v#{version}/mado-macOS-arm64.tar.gz"
-      sha256 "4000955c41799c839dbf1b4c3011ff1688bd31d0af88be282eec622a07ad9743"
+      sha256 "8043af62e1d2f726b34cb34c9f66fe74f9bc1750666351872318fb680f20cde6"
     end
 
     on_intel do
       url "https://github.com/akiomik/mado/releases/download/v#{version}/mado-macOS-x86_64.tar.gz"
-      sha256 "b8a3fb5cf3e84747c12a848fb87ce08c1c09dc5a957c1f74733bea6cb7f9d560"
+      sha256 "c7d0cc6665ca9d6535219a790222913a6d92d7a170aa4e7665cec4c2a1427646"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/akiomik/mado/releases/download/v#{version}/mado-Linux-gnu-arm64.tar.gz"
-      sha256 "ffcdd4845329a69bc729c0242abb6163f23495c0b04dbbaf608cbed43a2f4976"
+      sha256 "cb7df9feff1c117eeedc7b33267d853980192b7cffceb723794f74d54cde1522"
     end
 
     on_intel do
       url "https://github.com/akiomik/mado/releases/download/v#{version}/mado-Linux-gnu-x86_64.tar.gz"
-      sha256 "aad845cd23c8c0417cdf87b8376b75e131c38e1cb890124790567735306de6d7"
+      sha256 "403f72360876d42301b79f879344ba27bf320ca885069db2c697cd21acaf1f4d"
     end
   end
 
